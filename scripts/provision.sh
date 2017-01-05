eb32945eee0ed2a9670a20ad4be81b21be428ae1
gitsomecode <sjbitcode@gmail.com>
2017-01-05 01:26:54 -0500

    initial commit

---
+++ scripts/provision.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get -y upgrade
+
+sudo apt-get install -y python3-pip
+sudo apt-get install -y nginx
+sudo apt-get install -y tree
+sudo apt-get install -y git
+sudo apt-get install libpq-dev postgresql postgresql-contrib
