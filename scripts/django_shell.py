af8d9b734d55281e25bd7c24e3a324a5fcef38a5
gitsomecode <sjbitcode@gmail.com>
2017-01-06 02:32:59 -0500

    Fix model shortcut name

--- scripts/django_shell.py
+++ scripts/django_shell.py
@@ -34,6 +34,6 @@ if __name__ == '__main__':
 
     # Easy model functions.
     ph = lambda: Phrase.objects.all()
-    tl = lambda: TranslateEvent.objects.all()
+    te = lambda: TranslateEvent.objects.all()
 
     print_header()
