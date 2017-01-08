57f02b938858e81603c4dd13c11f10fe74f40507
gitsomecode <sjbitcode@gmail.com>
2017-01-07 23:28:29 -0500

    Add y flag to provision

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -8,4 +8,4 @@ sudo apt-get install -y python3-pip
 sudo apt-get install -y nginx
 sudo apt-get install -y tree
 sudo apt-get install -y git
-sudo apt-get install libpq-dev postgresql postgresql-contrib
+sudo apt-get install -y libpq-dev postgresql postgresql-contrib
